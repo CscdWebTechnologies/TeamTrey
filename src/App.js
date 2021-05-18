0f111bf7877f929aa1d1a6e87548f62b04acbf6f
kdrighteo <72208611+kdrighteo@users.noreply.github.com>
2021-05-18 10:01:40 +0000

    react

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
+//import logo from './logo.svg';
 import './App.css';
-import Portfolio from './portfolio.js';
-import Earn from './earn.js';
-import CoinTable from './table.js';
-import Trust from './Trust';
-import Stats from './Stats';
-import GetStarted from './GetStarted';
-import StartEarning from './StartEarning';
+import Hero from './hero.js';
+import NavBar from './navigationbar.js';
 
 function App() {
   return (
     <div className="bg-white h-screen">
-
-      {/* Ehmana */}
-      <CoinTable />
-      <Earn />
-      <Portfolio />
-      <Trust />
-
-      {/* Othniel */}
-      <Stats />
-      <GetStarted />
-      <StartEarning />
-
-
+      <NavBar />
+      <Hero />
+    
     </div>
   );
 }
