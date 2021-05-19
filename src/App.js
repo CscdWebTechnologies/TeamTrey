5353b0d1ef36294d7d9d4b426650e3c3591628cb
EhMaNa <68133399+EhMaNa@users.noreply.github.com>
2021-05-19 19:55:45 +0000

    App.js
    main app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+//import logo from './logo.svg';
+import './App.css';
+import Portfolio from './portfolio.js';
+import Earn from './earn.js';
+import CoinTable from './table.js';
+import Trust from './trust';
+
+
+function App() {
+  return (
+    <div className="bg-white h-screen">
+      <CoinTable />
+      <Earn />
+      <Portfolio />
+      <Trust />
+    
+      
+    </div>
+  );
+}
+
+export default App;
