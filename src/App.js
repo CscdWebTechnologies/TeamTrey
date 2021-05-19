00b6957afc6e1bebdaa6495bbf95560a9bd448f9
kdrighteo <72208611+kdrighteo@users.noreply.github.com>
2021-05-19 21:39:40 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import HeroSection from './hero.js';
+import NavigationBar from './navigationbar.js';
+
+
+function App() {
+  return (
+    <div className="bg-white h-screen">
+     
+      <NavigationBar />
+      <HeroSection />
+
+
+    </div>
+  );
+}
+
+export default App;
