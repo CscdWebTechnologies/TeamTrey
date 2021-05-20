b49cd79d51c82fd4a44beed1a6d3876dd97a6107
Othniel16 <othnielussher16@gmail.com>
2021-05-20 00:47:26 +0000

    Add bluebar (stats section) and all components below it

---
+++ src/GetStarted.js
@@ -0,0 +1,36 @@
+import classes from './Getstarted.module.css'
+import account from './images/Account.png'
+import bank from './images/Bank.png'
+import hand from './images/Hand.png'
+
+
+
+
+function Getstarted() {
+    return(
+        <section className={classes.sections}>
+            <div className={classes.mainDiv}>
+                <h2 className={classes.heading1}>Get started in a few minutes</h2>
+                <h2 className={classes.heading2}>Coinbase supports a variety of the most popular digital currencies.</h2>
+                <div className={classes.divIcons}>
+                    <div>
+                        <img src ={account} className={classes.center}></img>
+                        <h2 className={classes.innerText}>Create an account </h2>
+                    </div>
+                    <div className={classes.horizontalbar}></div>
+                    <div>
+                        <img src ={bank} className={classes.center}></img>
+                        <h2 className={classes.innerText}>Link your bank account</h2>
+                    </div>
+                    <div className={classes.horizontalbar}></div>
+                    <div>
+                        <img src ={hand} className={classes.center}></img>
+                        <h2 className={classes.innerText}>Start buying & selling</h2>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Getstarted;
